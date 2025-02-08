ee23efa0fcffeec29ddc4f2dac731cb7431f31b0
parkgang <ruddms936@naver.com>
2025-02-08 15:55:06 +0900

    feat: vscode 제외 vanilla-nvim 설정 모두 가져옴

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,9 @@
 -- [매크로 툴이 설치되지 않은 환경에서 fallback으로 사용될 한/영 전환문제 대응 설정](https://johngrib.github.io/blog/2017/05/04/input-source/#langmap%EC%9D%84-%EC%82%AC%EC%9A%A9%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95)
 vim.opt.langmap =
   "ㅁa,ㅠb,ㅊc,ㅇd,ㄷe,ㄹf,ㅎg,ㅗh,ㅑi,ㅓj,ㅏk,ㅣl,ㅡm,ㅜn,ㅐo,ㅔp,ㅂq,ㄱr,ㄴs,ㅅt,ㅕu,ㅍv,ㅈw,ㅌx,ㅛy,ㅋz"
+
+--
+--
+-- 레지스터
+-- <C-a> 단축키를 희생했음으로 편하게 전체 선택을 위한 메크로
+vim.fn.setreg("a", "ggVG")
