36acef6867e808b6ba1c17bcf075d5b5a92a5550
parkgang <ruddms936@naver.com>
2025-02-08 20:40:59 +0900

    feat: neo-tree 숨김 파일 기본 활성화

---
+++ lua/plugins/custom.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- 기본적으로 숨김 파일 보이기
+        },
+      },
+    },
+  },
+}
