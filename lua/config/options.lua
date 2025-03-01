f0a17f05697f245311a1bfc4661715a8b37d3f2d
parkgang <ruddms936@naver.com>
2025-03-01 21:12:30 +0900

    feat: Neovide 설정

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -27,3 +27,35 @@ if vim.g.vscode then
   vim.keymap.set("n", "zO", "<Cmd>call VSCodeNotify('editor.unfoldRecursively')<CR>")
   vim.keymap.set("n", "za", "<Cmd>call VSCodeNotify('editor.toggleFold')<CR>")
 end
+
+--
+--
+-- Neovide
+if vim.g.neovide then
+  -- 기본적으로 Nerd Font 찾아서 적용해줌
+  -- vim.o.guifont = "MesloLGS Nerd Font:h14"
+
+  -- [cmd+s, cmd+c, cmd+v 복붙 단축키](https://neovide.dev/faq.html#how-can-i-use-cmd-ccmd-v-to-copy-and-paste)
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+  vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+
+  -- [확대/축소 단축키](https://neovide.dev/faq.html#how-can-i-dynamically-change-the-scale-at-runtime)
+  vim.g.neovide_scale_factor = 1.2
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.25)
+  end)
+end
